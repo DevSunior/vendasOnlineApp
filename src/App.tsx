925b85c993aa0c1e439daa8fbe834082cee84819
DevSunior <devsunior@gmail.com>
2024-02-13 12:20:14 -0300

    Adding Styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView, StyleSheet, Text } from "react-native";
+import styled from "styled-components/native";
+
+// Exemplo de declaração styled components
+const TextNew = styled.Text`
+    color: aqua;
+`
 
 const App = () => {
 
     return (
         <SafeAreaView>
-            <Text>Testando</Text>
+            <Text style={styles.container}>Testando</Text>
+            <TextNew>NOVO TESTE</TextNew>
         </SafeAreaView>
     )
 };
+
+const styles = StyleSheet.create({
+    container: {
+        color: 'blue'
+    }
+})
 
 export default App;
